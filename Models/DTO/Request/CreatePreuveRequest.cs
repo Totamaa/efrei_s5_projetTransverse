03e9ae8289eda9391c47e18bb2d66227a3a46837
Matteo Calderaro <88966144+Totamaa@users.noreply.github.com>
2023-12-30 10:51:30 +0100

    refacto(Models): refacto nullable/required (#20)
    Co-authored-by: Matteo Calderaro <calderaro.matteo@proton.me>

--- Models/DTO/Request/CreatePreuveRequest.cs
+++ Models/DTO/Request/CreatePreuveRequest.cs
@@ -2,19 +2,22 @@ using Projet.Models.Entity;
 
 namespace Projet.Models.DTO.Request;
 
-public class CreatePreuveRequest
+public class CreatePreuveRequest(
+    int dossierId,
+    int typePreuveId,
+    string? contenu = null
+)
 {
-    public int? DossierId;
-    public int? TypePreuveId;
-    public string? Contenu;
+    public int DossierId { get; set; } = dossierId;
+    public int TypePreuveId { get; set; } = typePreuveId;
+    public string? Contenu { get; set; } = contenu;
 
     public PreuveEntity ToEntity()
     {
-        return new PreuveEntity
-        {
-            DossierId = (int)DossierId!,
-            TypePreuveId = (int)TypePreuveId!,
-            Contenu = Contenu
-        };
+        return new PreuveEntity(
+            DossierId,
+            TypePreuveId,
+            Contenu
+        );
     }
 }
